09964dbf7fa9ade7d69e08aa928b346efad8f891
jaimezzapata <zapataval2304@gmail.com>
2025-02-25 10:23:39 -0500

    Se crea primer componente con estilos básicos

---
+++ src/PanelControl.jsx
@@ -0,0 +1,25 @@
+export const PanelControl = () => {
+  return (
+    <section className="panel-control">
+      <aside className="panel-control-lateral">
+        <div>
+          <img src="" alt="Logo" />
+        </div>
+        <nav>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </nav>
+      </aside>
+      <div className="panel-control-contenido">
+        <h1>Mensaje de bienvenida</h1>
+      </div>
+    </section>
+  )
+}
+
+export default PanelControl
+
